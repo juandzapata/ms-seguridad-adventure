c8aa11bd29187e1eaa1f89c01f7eefa1859e8d8c
julianagomez21 <jgomezgal25@gmail.com>
2022-10-31 18:53:06 -0500

    Factor de doble autenticacion

--- src/services/seguridad-usuario.service.ts
+++ src/services/seguridad-usuario.service.ts
@@ -5,10 +5,12 @@ import fetch from 'node-fetch';
 import {Keys} from '../config/keys';
 import {CredencialesLogin, CredencialesRecuperarClave} from '../models';
 import {UsuarioRepository} from '../repositories';
+import {VerificacionCodigoRepository} from '../repositories/verificacion-codigo.repository';
 import {JwtService} from './jwt.service';
 var generator = require('generate-password');
 var md5 = require('crypto-js/md5');
 const params = new URLSearchParams();
+const paramsSMS = new URLSearchParams();
 
 @injectable({scope: BindingScope.TRANSIENT})
 export class SeguridadUsuarioService {
@@ -17,47 +19,79 @@ export class SeguridadUsuarioService {
     private usuarioRepository: UsuarioRepository,
     @service(JwtService)
     private servicioJwt: JwtService,
-  ) {}
+    @repository(VerificacionCodigoRepository)
+    private codigoRepository: VerificacionCodigoRepository
+  ) { }
 
   /**
-   * Método para la autenticación de usuarios
+   * Metodo para la autenticacion de usuarios
    * @param credenciales credenciales de acceso
-   * @returns una cadena con el token cuando todo está bien, o una cadena vacia cuando no coinciden las credenciales
+   * @returns cadena con el token cuando todo esta bien, o una cadena vacia cuando no coinciden las credenciales.
    */
   async identificarUsuario(credenciales: CredencialesLogin): Promise<string> {
-    let respuesta = '';
-
-    //Para que el usuario pueda ingresar la clave sin cifrar
-    let clave = credenciales.clave;
-    let claveCifrada = this.cifrarCadena(clave);
+    let respuesta = "";
 
     const usuario = await this.usuarioRepository.findOne({
       where: {
         correo: credenciales.correo,
-        clave: claveCifrada,
-      },
+        clave: credenciales.clave
+      }
     });
 
-    console.log(credenciales.correo);
-    console.log(credenciales.clave);
     console.log(usuario);
 
+
     if (usuario) {
-      // Creación del token y asignación a respuesta
-      const datos = {
-        nombre: `${usuario.nombres} ${usuario.apellidos}`,
-        correo: usuario.correo,
-        rol: usuario.rolId,
-      };
       try {
-        respuesta = this.servicioJwt.crearToken(datos);
-        console.log(respuesta);
+        let codigo = this.crearCodigoAleatorio();
+        let mensaje = `¡Hola ${usuario.nombres}! <br /> Tu código de verificación es: ${codigo}`
+
+        params.append('hash_validator', 'Admin12345@notificaciones.sender');
+        params.append('destination', usuario.correo);
+        params.append('subject', Keys.mensajeAsuntoVerificacion);
+        params.append('message', mensaje);
+
+        let r = "";
+
+        //console.log(params);
+        //console.log(Keys.urlEnviarCorreo);
+
+        await fetch(Keys.urlEnviarCorreo, {method: 'POST', body: params}).then(async (res: any) => {
+          r = await res.text();
+        });
+
+        console.log("aqui");
+
+
+        mensaje = `¡Hola ${usuario.nombres}! Tu código de verificación es: ${codigo}`
+        paramsSMS.append('hash_validator', 'Admin12345@notificaciones.sender');
+        paramsSMS.append('message', mensaje);
+        paramsSMS.append('destination', usuario.celular);
+
+        //console.log("soy tu puto problema");
+
+        await fetch(Keys.urlEnviarSMS, {method: 'POST', body: paramsSMS}).then(async (res: any) => {
+          r = await res.text();
+        });
+
+        console.log("estoy aqui");
+
+        const codigoVerificacion = await this.codigoRepository.create({
+          usuarioId: usuario.correo,
+          codigo: codigo,
+          estado: false
+        });
+
+        //console.log(codigoVerificacion);
+        respuesta = 'Código enviado.'
+
       } catch (err) {
         throw err;
       }
     } else {
-      console.log('No se está retornando ningún token');
+      return "Las credenciales no coinciden."
     }
+
     return respuesta;
   }
 
@@ -77,6 +111,20 @@ export class SeguridadUsuarioService {
   }
 
   /**
+   * Genera un codigo aleatorio
+   * @returns codigo generado
+   */
+  crearCodigoAleatorio(): string {
+    let codigo = generator.generate({
+      length: 4,
+      numbers: true,
+    });
+
+    console.log(codigo);
+    return codigo;
+  }
+
+  /**
    * Cifra una cadena
    * @param cadena Una cadena para cifrar
    * @returns la cadena cifrada
@@ -90,7 +138,7 @@ export class SeguridadUsuarioService {
    * Se recupera una clave generandola aleatoriamente y enviandola por correo
    * @param credenciales credenciales del usuario a recuperar la clave
    */
-   async recuperarClave(credenciales: CredencialesRecuperarClave): Promise<boolean> {
+  async recuperarClave(credenciales: CredencialesRecuperarClave): Promise<boolean> {
 
 
     let usuario = await this.usuarioRepository.findOne({
@@ -132,9 +180,9 @@ export class SeguridadUsuarioService {
     }
   }
 
-  async correoPrimerContraseña(correo: string, claveGenerada: string): Promise<Boolean>{
+  async correoPrimerContraseña(correo: string, claveGenerada: string): Promise<Boolean> {
     let usuario = await this.usuarioRepository.findOne({
-      where:{
+      where: {
         correo: correo
       }
     });
@@ -167,5 +215,50 @@ export class SeguridadUsuarioService {
       throw new HttpErrors[400]('Error con la confirmación de la creación del usuario');
     }
 
+  }
+
+  /**
+   * Funcion para validar el codigo de usuario
+   * @param codigo tiene el codigo generado y el usuario al que pertenece
+   * @returns un JWT si el codigo es valido, si no retorna una cadena vacia
+   */
+  async validarCodigo(codigo: string): Promise<string> {
+    let respuesta = "";
+    let verificar = await this.codigoRepository.findOne({
+      where: {
+        codigo: codigo
+      }
+    });
+
+    if (verificar) {
+
+      if (verificar.estado == false) {
+        verificar.estado = true;
+        this.codigoRepository.updateById(verificar._id, verificar);
+        let usuario = await this.usuarioRepository.findOne({
+          where: {
+            correo: verificar.usuarioId
+          }
+        });
+
+        if (usuario) {
+
+          const datos = {
+            nombre: `${usuario.nombres} ${usuario.apellidos}`,
+            correo: usuario.correo,
+            rol: usuario.rolId
+          };
+
+          try {
+            respuesta = this.servicioJwt.crearToken(datos);
+          } catch (error) {
+            throw error;
+          }
+        }
+      } else {
+        respuesta = 'Código inválido.'
+      }
+    }
+    return respuesta;
   }
 }
